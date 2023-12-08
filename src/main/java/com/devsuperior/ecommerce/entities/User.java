2a01c59cd794abed9f7568b05e5b363ab7f0b17a
lucasrissy <rissiandrade@gmail.com>
2023-12-08 10:45:06 +0000

    git commit -m"create orderItem entity"

--- src/main/java/com/devsuperior/ecommerce/entities/User.java
+++ src/main/java/com/devsuperior/ecommerce/entities/User.java
@@ -84,7 +84,7 @@ public class User {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(Object o) {//jjhj
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
